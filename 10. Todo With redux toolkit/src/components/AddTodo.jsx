2105fb2710abceb6877cd29f0dedefd47393ca3f
Rifna <102509964+RifnaC@users.noreply.github.com>
2024-05-17 21:20:32 +0530

    updated the project

--- 10. Todo With redux toolkit/src/components/AddTodo.jsx
+++ 10. Todo With redux toolkit/src/components/AddTodo.jsx
@@ -5,16 +5,24 @@ import { addTodo } from '../features/TodoSlice'
 
 const AddTodo = () => {
     const [data, setData] = useState('');
-    const dispatch = useDispatch(addTodo());
+    const dispatch = useDispatch();
     function handleSubmit(e){
         e.preventDefault();
-        dispatch(data);
+        dispatch(addTodo(data));
         setData('')
     }
   return (
     <>
-        <input value={data} onChange={(e)=>e.target.value}/>
-        <button onClick={handleSubmit}>Add</button>
+      <form onSubmit={handleSubmit} className='space-x-3 mt-12'>
+        <input 
+        type='text' 
+        className='bg-gray-800 rounded border border-gray-700 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-out'
+        placeholder='Enter a Todo ...' 
+        value={data} 
+        onChange={(e)=>setData(e.target.value)}/>
+        <button type='submit' 
+        className='text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg'>Add Tod</button>
+      </form>
     </>
   )
 }
--- 10. Todo With redux toolkit/src/components/Todo.jsx
+++ 10. Todo With redux toolkit/src/components/Todo.jsx
@@ -1,10 +1,25 @@
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
+import { removeTodo } from "../features/TodoSlice";
+
 
 
 const Todo = () => {
-    useSelector(state => state.todos)
+    const todos = useSelector(state => state.todos)
+    const dispatch = useDispatch();
   return (
-    <div>Todo</div>
+    <>
+        <h1 className="text-3xl font-bold text-black mt-3">Todo</h1>  
+        <ul className="list-none">
+            {todos.map((todo)=>(
+                <li className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
+                key={todo.id}>
+                    <div className="text-white">{todo.title}</div>
+                    <button onClick={()=>dispatch(removeTodo(todo.id))} className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md">Delete</button>
+                </li>
+            ))}
+        </ul>
+        
+    </>
   )
 }
 
