dda4febec5b4158caa10279a78183bffe5957ec6
Rifna <102509964+RifnaC@users.noreply.github.com>
2024-05-18 09:55:50 +0530

    create the new project

---
+++ 11. Tic tac toe/src/components/Square.jsx
@@ -0,0 +1,11 @@
+const Square = () => {
+  return (
+    <div className="flex flex-col">
+        <div className="text-2xl font-bold my-2">Tic Tac Toe</div>
+        <button className="border-2 border-black py-1 px-3">X</button>
+    </div>
+    
+  )
+}
+
+export default Square
